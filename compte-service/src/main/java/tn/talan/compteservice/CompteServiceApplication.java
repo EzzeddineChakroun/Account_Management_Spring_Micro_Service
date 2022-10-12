54267419be2e2641e4672d7cd898d01e0afe571e
cezzeddinetalan <cezzeddine.talan@gmail.com>
2022-10-12 12:27:38 +0100

    adding compte service

---
+++ compte-service/src/main/java/tn/talan/compteservice/CompteServiceApplication.java
@@ -0,0 +1,35 @@
+package tn.talan.compteservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tn.talan.compteservice.entities.Compte;
+import tn.talan.compteservice.enums.TypeCompte;
+import tn.talan.compteservice.repositories.CompteRepository;
+
+import java.util.Date;
+
+
+@SpringBootApplication
+public class CompteServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CompteServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CompteRepository compteRepository){
+        return args -> {
+          compteRepository.save(new Compte(null,1500,new Date(), TypeCompte.COURANT));
+          compteRepository.save(new Compte(null,6430,new Date(), TypeCompte.EPARGNE));
+          compteRepository.save(new Compte(null,5460,new Date(), TypeCompte.COURANT));
+          compteRepository.findAll().forEach(cp -> {
+              System.out.println(cp.getType());
+              System.out.println(cp.getSolde());
+          });
+        };
+    }
+
+}
